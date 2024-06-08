0d50a94cc949c17c345c0166b51a7a927cdb5fe6
Charles-Elie Hervy <75300601+chervys@users.noreply.github.com>
2024-06-08 13:12:23 +0200

    Up...

--- P1/setup_build_run_images.sh
+++ P1/setup_build_run_images.sh
@@ -7,11 +7,11 @@ command_exists() {
 
 if ! command_exists gns3; then
     echo "Installing GNS3..."
-    sudo apt-get update
-    sudo apt-get install -y python3 python3-pip python3-pyqt5 qemu-kvm qemu-utils libvirt-clients libvirt-daemon-system virtinst bridge-utils
+    sudo apt update
+    sudo apt install -y python3 python3-pip python3-pyqt5 qemu-kvm qemu-utils libvirt-clients libvirt-daemon-system virtinst bridge-utils
     sudo add-apt-repository ppa:gns3/ppa
-    sudo apt-get update
-    sudo apt-get install -y gns3-gui gns3-server
+    sudo apt update
+    sudo apt install -y gns3-gui gns3-server
 else
     echo "GNS3 is already installed."
 fi
